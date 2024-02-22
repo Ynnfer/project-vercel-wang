266d20413520abbd714b7ffc368db3acbfe3905a
WANG Nuoxi <chuya32384@gmail.com>
2024-02-22 09:30:07 +0100

    add NEXTAUTH

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
-import { AppProps } from "next/app";
+// import { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+// const App = ({ Component, pageProps }: AppProps) => {
+//   return (
+//     <Component {...pageProps} />
+//   );
+// };
+
+
+// export default App;
+
+// import { SessionProvider } from 'next-auth/react';
+// import { AppProps } from 'next/app';
+
+// const App = ({ Component, pageProps }: AppProps) => {
+//   return (
+//     <SessionProvider session={pageProps.session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   );
+// };
+
+// export default App;
+
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+
+interface MyAppProps extends AppProps {
+  pageProps: {
+    session: Session | null | undefined; // 这里根据实际情况调整 Session 的类型
+  };
+}
+
+const App = ({ Component, pageProps }: MyAppProps) => {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
